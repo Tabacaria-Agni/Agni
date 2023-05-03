47149c8f5854951a31f99eac641b9135ee6c2454
MateusGutierrez <mateusgutierrez9@gmail.com>
2023-05-03 15:30:01 -0300

    Header and footer atualization

--- my-app/src/components/Header/style.ts
+++ my-app/src/components/Header/style.ts
@@ -1,23 +1,46 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-height:150px;
-width:100%;
-
+height:100px;
+width: 100%;
 border-bottom: 1px solid var(--color-primary);
-display:flex;
-justify-content:space-evenly;
-align-items:center;
 
-color:var(--grey-0);
-
-
-
-img{
-    width:100px
+.containerHeader{
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    color:var(--grey-0);
+    height: 100px;
+    width: 90%;
+    margin-left: 5%;
 }
 
-@media(max-width:400px){
-    font-size:12px;
+h1{
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 29px;
+}
+
+img{
+    width:70px;
+}
+
+@media(min-width:1000px){
+    height:196px;
+    .containerHeader{
+        width: 80%;
+        margin-left: 10%;
+        height:196px;
+    }
+    h1{
+        font-style: normal;
+        font-weight: 400;
+        font-size: 42px;
+        line-height: 51px;
+    }
+    img{
+        width: 170px;
+    }
 }
 `
